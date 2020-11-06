01535d3f7bd0b6a986d4483cfdc8289495415a1c
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2020-11-06 17:33:39 -0500

    Comments and clarifications to the cmd script
    I added comments to clarify what each command does.

--- final.cmd
+++ final.cmd
@@ -1,12 +1,33 @@
-type C:\files\mysql\project2begin.sql > C:\files\mysql\final.sql
-type C:\files\mysql\input.txt > C:\files\mysql\temp.txt
-echo.>> C:\files\mysql\input.txt
-echo NotAnID>> C:\files\mysql\input.txt
-@powershell -Command "(get-content C:\files\mysql\input.txt) -join \"',`n'\" > C:\files\mysql\input2.txt"
-type C:\files\mysql\input2.txt >> C:\files\mysql\final.sql
-type C:\files\mysql\project2end.sql >> C:\files\mysql\final.sql
-type C:\files\mysql\temp.txt > C:\files\mysql\input.txt
-del /f C:\files\mysql\input.txt
-sqlcmd -S 168.168.168.168\SQL2015 -U username -P password -i C:\files\mysql\final.sql -o C:\files\mysql\results.txt
+:: Go to the drectory of the files. Change your directory accordingly
+cd C:\files\mysql
+
+:: Copy first part of the query to the final SQL file
+type project2begin.sql > final.sql
+
+:: Copy input values to a temporary file
+type input.txt > temp.txt
+
+:: Alter the input values to fit in SQL query. 
+:: PowerShell command is used for find and replace functionality
+echo.>> input.txt
+echo NotAnID>> input.txt
+@powershell -Command "(get-content input.txt) -join \"',`n'\" > input2.txt"
+
+:: Copy the altered input to the final SQL file
+type input2.txt >> final.sql
+
+:: Copy the last part of the query to the final SQL file
+type project2end.sql >> C:\files\mysql\final.sql
+
+:: Restore the initial input.txt file
+type temp.txt > input.txt
+
+:: Delete the temporarily created files
+del /f input.txt
+del /f input2.txt
+
+:: Send the query to the server and get the output in results.txt
+sqlcmd -S 168.168.168.168\SQL2015 -U username -P password -i final.sql -o results.txt
 echo done 
  
+ exit
